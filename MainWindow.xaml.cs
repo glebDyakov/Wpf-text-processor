6d151d7039eb7f0e81e6a6e21a4c7704bcf7cb1a
glebDyakov <67467704+glebDyakov@users.noreply.github.com>
2021-01-08 11:24:44 +0300

    finish text editor

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -46,19 +47,80 @@ namespace ExampleAppThree
 
         private void saveFile_Click(object sender, RoutedEventArgs e)
         {
-            SaveFileDialog sfd = new SaveFileDialog();
-            sfd.ShowDialog();
+            saveToFile();
         }
 
         private void createNewFile_Click(object sender, RoutedEventArgs e)
         {
+            if (textBox.Text != "") {
+                saveToFile();
+            }
+            textBox.Text = "";
+        }
 
+        private void saveToFile()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            bool? res = sfd.ShowDialog();
+            if (res != false)
+            {
+
+                using (Stream s = File.Open(sfd.FileName, FileMode.OpenOrCreate))
+                {
+                    using (StreamWriter sw = new StreamWriter(s))
+                    {
+                        sw.Write(textBox.Text);
+                    }
+                }
+            }
         }
 
         private void openNewFile_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            ofd.ShowDialog();
+            bool? res = ofd.ShowDialog();
+            if (res != false) { 
+                Stream myStream;
+                if ((myStream = ofd.OpenFile()) != null) {
+                    string file_name = ofd.FileName;
+                    string file_text = File.ReadAllText(file_name);
+                    textBox.Text = file_text;
+                }
+            }
+        }
+
+        private void selectFontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontSize = selectFontSize.SelectedItem.ToString();
+            fontSize = fontSize.Substring(fontSize.Length - 2);
+            switch (fontSize) {
+                case "10":
+                    textBox.FontSize = 10;
+                    break;
+                case "14":
+                    textBox.FontSize = 14;
+                    break;
+                case "16":
+                    textBox.FontSize = 16;
+                    break;
+                case "20":
+                    textBox.FontSize = 20;
+                    break;
+                case "24":
+                    textBox.FontSize = 24;
+                    break;
+                case "32":
+                    textBox.FontSize = 32;
+                    break;
+                case "48":
+                    textBox.FontSize = 48;
+                    break;
+            }
+        }
+
+        private void createNewFile_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
